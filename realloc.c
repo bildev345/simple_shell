0296ada9225356b2fb39b399df263c9f13d75e86
abdelmajid49 <majidabdel21@gmail.com>
2023-05-14 09:42:20 -0700

    realloc.c

---
+++ realloc.c
@@ -0,0 +1,62 @@
+#include "shell.h"
+
+/**
+ **_memset - fills memory with a constant byte
+ *@s: the pointer to the memory area
+ *@b: the byte to fill *s with
+ *@n: the amount of bytes to be filled
+ *Return: (s) a pointer to the memory area s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		s[i] = b;
+	return (s);
+}
+
+/**
+ * ffree - frees a string of strings
+ * @pp: string of strings
+ */
+void ffree(char **c)
+{
+	char **a = c;
+
+	if (!c)
+		return;
+	while (*c)
+		free(*c++);
+	free(a);
+}
+
+/**
+ * _realloc - reallocates a block of memory
+ * @ptr: pointer to previous malloc'ated block
+ * @old_size: byte size of previous block
+ * @new_size: byte size of new block
+ *
+ * Return: pointer to da ol'block nameen.
+ */
+void *_realloc(void *point, unsigned int old_size, unsigned int new_size)
+{
+	char *p;
+
+	if (!point)
+		return (malloc(new_size));
+	if (!new_size)
+		return (free(point), NULL);
+	if (new_size == old_size)
+		return (point);
+
+	p = malloc(new_size);
+	if (!p)
+		return (NULL);
+
+	old_size = old_size < new_size ? old_size : new_size;
+	while (old_size--)
+		p[old_size] = ((char *)point)[old_size];
+	free(point);
+	return (p);
+}
